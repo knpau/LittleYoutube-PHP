5d110d5db276b2ea24b384c03949cdb5e1184768
StefansArya <arya.cipta@yahoo.com>
2017-08-07 13:27:40 +0700

    Fix travis test

--- tests/index.php
+++ tests/index.php
@@ -1,7 +1,7 @@
 <?php
 	// If you installed via composer, just use this code to require autoloader on the top of your projects.
-	//require_once './../vendor/autoload.php';
-	require_once "./../LittleYoutube.php";
+	//require_once __DIR__."/../vendor/autoload.php';
+	require_once __DIR__."/../LittleYoutube.php";
 	
 	use ScarletsFiction\LittleYoutube;
 	
