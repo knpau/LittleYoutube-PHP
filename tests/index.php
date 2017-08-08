63a96818d8c5ea7296c4e91fab5297f386fbd938
StefansArya <arya.cipta@yahoo.com>
2017-08-09 04:29:39 +0700

    Support live stream video

--- tests/index.php
+++ tests/index.php
@@ -1,6 +1,6 @@
 <?php
 	// If you installed via composer, just use this code to require autoloader on the top of your projects.
-	//require_once __DIR__."/../vendor/autoload.php';
+	//require_once __DIR__."/../vendor/autoload.php";
 	require_once __DIR__."/../LittleYoutube.php";
 	use ScarletsFiction\LittleYoutube;
 
@@ -9,7 +9,7 @@
 
 	$video = $LittleYoutube->video("https://www.youtube.com/watch?v=R1RonAlzvZk");
 	//$video->getImage();
-	//print_r($video->parseSubtitle(0, true));
+	//print_r($video->data);
 
 	$channel = $LittleYoutube->channel("https://www.youtube.com/user/yifartofmagic/");
 	//print_r($channel->data);
