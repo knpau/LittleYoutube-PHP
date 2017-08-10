2d24d2b7fab6b9bd8d69fe425281588b8d61d189
StefansArya <arya.cipta@yahoo.com>
2017-08-11 02:41:24 +0700

    Update Tests

--- tests/index.php
+++ tests/index.php
@@ -2,20 +2,29 @@
 	// If you installed via composer, just use this code to require autoloader on the top of your projects.
 	//require_once __DIR__."/../vendor/autoload.php";
 	require_once __DIR__."/../LittleYoutube.php";
-	use ScarletsFiction\LittleYoutube;
 
-	$LittleYoutube = new LittleYoutube();
-	$LittleYoutube->settings['loadVideoSize'] = true;
+	use \ScarletsFiction\LittleYoutube;
 
-	$video = $LittleYoutube->video("https://www.youtube.com/watch?v=R1RonAlzvZk");
+	$error = '';
+
+	$video = LittleYoutube::video("https://www.youtube.com/watch?v=R1RonAlzvZk");
+	$error .= $video->error."\n";
 	//$video->getImage();
 	//print_r($video->data);
 
-	$channel = $LittleYoutube->channel("https://www.youtube.com/user/yifartofmagic/");
+	$channel = LittleYoutube::channel("https://www.youtube.com/user/yifartofmagic/");
+	$error .= $channel->error."\n";
 	//print_r($channel->data);
 
-	$playlist = $LittleYoutube->playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
+	$playlist = LittleYoutube::playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
+	$error .= $playlist->error."\n";
 	//print_r($playlist->data);
 
-	if($LittleYoutube->error) echo('Error: '.$LittleYoutube->error);
+	$search = LittleYoutube::search("nice movies");
+	//$search->next();
+	//$search->previous();
+	$error .= $search->error."\n";
+	//print_r($search->data);
+
+	if(str_replace("\n", '', $error)) echo('Error: '.$error);
 	else echo("All test script running");
