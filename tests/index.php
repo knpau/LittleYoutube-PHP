1103e79b7b8c413a40cb4937da84354c065c5f88
StefansArya <arya.cipta@yahoo.com>
2017-09-15 01:20:13 +0700

    Fix search result

--- tests/index.php
+++ tests/index.php
@@ -7,23 +7,23 @@
 
 	$error = '';
 
-	$video = LittleYoutube::video("https://www.youtube.com/watch?v=R1RonAlzvZk");
+	$video = LittleYoutube::video("https://www.youtube.com/watch?v=R1RonAlzvZk", array("loadVideoSize"=>true));
 	$error .= $video->error."\n";
 	//$video->getImage();
-	//print_r($video->data);
+	print_r($video->data);
 
-	$channel = LittleYoutube::channel("https://www.youtube.com/user/yifartofmagic/");
-	$error .= $channel->error."\n";
+	//$channel = LittleYoutube::channel("https://www.youtube.com/user/yifartofmagic/");
+	//$error .= $channel->error."\n";
 	//print_r($channel->data);
 
-	$playlist = LittleYoutube::playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
-	$error .= $playlist->error."\n";
+	//$playlist = LittleYoutube::playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
+	//$error .= $playlist->error."\n";
 	//print_r($playlist->data);
 
-	$search = LittleYoutube::search("nice movies");
+	//$search = LittleYoutube::search("nice movies");
 	//$search->next();
 	//$search->previous();
-	$error .= $search->error."\n";
+	//$error .= $search->error."\n";
 	//print_r($search->data);
 
 	if(str_replace("\n", '', $error)) echo('Error: '.$error);
