01d204c62a0708a1a33eb96082e8f12545c83e97
StefansArya <stefansarya1@gmail.com>
2018-11-10 10:27:27 +0700

    Update test case

--- tests/index.php
+++ tests/index.php
@@ -1,27 +1,16 @@
 <?php
-	// If you installed via composer, just use this code to require autoloader on the top of your projects.
-	require_once __DIR__."/../vendor/autoload.php";
-	//require_once __DIR__."/../LittleYoutube.php";
+// If you installed via composer, just use this code to require autoloader on the top of your projects.
+require_once __DIR__."/../vendor/autoload.php";
 
-	use \ScarletsFiction\LittleYoutube;
+use \LittleYoutube\LittleYoutube;
 
-	$error = '';
-	$video = LittleYoutube::video("https://www.youtube.com/watch?v=R1RonAlzvZk", ["temporaryDirectory"=>realpath(__DIR__."/../example/temp")]);
-	$video->getImage();
-	$error .= $video->error."\n";
-	//print_r($video->data);
+$video = LittleYoutube::video("https://www.youtube.com/watch?v=R1RonAlzvZk", ["temporaryDirectory"=>realpath(__DIR__."/../example/temp")]);
+$video->getImage();
 
-	$channel = LittleYoutube::channel("https://www.youtube.com/user/yifartofmagic/");
-	$error .= $channel->error."\n";
-	////print_r($channel->data);
+$channel = LittleYoutube::channel("https://www.youtube.com/user/yifartofmagic/");
 
-	$playlist = LittleYoutube::playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
-	$error .= $playlist->error."\n";
-	////print_r($playlist->data);
+$playlist = LittleYoutube::playlist("https://www.youtube.com/watch?v=692TvKPDaEU&list=UUa-iuHGLTxvkOChd4jnybiA");
 
-	$search = LittleYoutube::search("nice movies");
-	$error .= $search->error."\n";
-	//print_r($search->data);
+$search = LittleYoutube::search("nice movies");
 
-	if(str_replace("\n", '', $error)) echo('Error: '.$error);
-	else echo("All test script running");
+echo("All test script running");
